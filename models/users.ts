ceaf68ee8df3adfc4fd63e8083122785da6bb1ec
antonioIannotta <antonio.iannotta.ai26@gmail.com>
2024-11-24 21:29:25 +0100

    feat: first requirements completed

--- models/users.ts
+++ models/users.ts
@@ -3,17 +3,37 @@ import  mongoose, { Schema, Document } from 'mongoose';
 interface IUser extends Document {
     username: String,
     email: String,
-    salary: Number,
+    grossSalary: Number,
     city: String,
-    interests: Array<String>,
+    region: String | undefined,
+    country: String | undefined,
+    yoE: Number,
+    yoEwithinTheCompany: Number,
+    weekelyRemoteWorking: Boolean,
+    weekelyRemoteWorkingInDays: Number | undefined,
+    industry: String,
+    jobTitle: String,
+    ticketRestaurant: Boolean,
+    ticketRestaurantAmount: Number | undefined,
+    ticketRestaurantValue: Number | undefined
 }
 
 const userSchema: Schema = new Schema({
     'username': {type: String, required: true},
     'email': {type: String, required: true},
-    'salary': {type: Number, required: true},
+    'grossSalary': {type: Number, required: true},
     'city': {type: String, required: true},
-    'interests': {type: Array<String>, required: true}
+    'region': {type: String, required: false},
+    'country': {type: String, required: false},
+    'yoE': {type: Number, required: true},
+    'yoEwithinTheCompany': {type: Number, required: true},
+    'weekelyRemoteWorking': {type: Boolean, required: true},
+    'weekelyRemoteWorkingInDays': {type: Number, required: false},
+    'industry': {type: String, required: true},
+    'jobTitle': {type: String, required: true},
+    'ticketRestaurant': {type: Boolean, required: true},
+    'ticketRestaurantAmount': {type: Number, required: false},
+    'ticketRestaurantValue': {type: Number, required: false}
 }, {collection: 'users'});
 
 const User = mongoose.model<IUser>('User', userSchema);
---
+++ requirements.md
@@ -0,0 +1,23 @@
+# Input Requirements
+- The user must be allowed to sign up into the app with the Oauth authtentication mechanism.
+- The user, either during the registration process or in a further moment, must be able to insert or modify the following information:
+    - Gross salary
+    - City, Region, State
+    - Current Company
+    - Years of Experience within the company
+    - Years of experience in general
+    - Smart working (Y/N). If Y specify the number of days
+    - Industry
+    - Job title
+    - Ticket Restaurant, if yes, the correspondent amount.
+
+# DATA VISUALIZATION
+
+Within the home page, the user can visualize the several information from the following:
+- Avg gross salary, in general, for that city, region and state
+- Avg gross salary within that industry, again the user can select if the visualization should be performed against the city, the region or the state.
+- As in the previous point, but for the job title
+- The user can visualize not only the average but also advanced statistical graphs about the distribution of the salaries within city, industry and job role.
+- The user can select multiple cities and perform a comparaison
+- The statistical information should be provided considering the YoE and the SmartWorking too.
+- The choice of the user about the data to visualize must be guided by the data already stored.
